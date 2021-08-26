b452988a3d33512165cfcac1f9874717b9096f0c
Guilherme Cardoso <guilherme_wa@hotmail.com>
2021-08-26 17:45:08 -0300

    aula java

---
+++ Teste2/src/Teste2/Teste2.java
@@ -0,0 +1,33 @@
+package Teste2;
+
+public class Teste2 {
+
+	public static void main(String[] args) {
+
+		// String nome = "Guilherme Cardoso";
+		// String cpf = "373.004.108-80";
+		// String telefone = "(12)991569775";
+		// String endereco = "Rua Teresina, 54";
+		// int idade = 33;
+
+		// String saida = "Meu nome é = " + nome + " minha idade é : " + idade +
+		// " e o CPF é = " + cpf + " meu telefone é : " + telefone +
+		// " e meu endereço é : " + endereco;
+
+		// System.out.println(saida);
+
+		int mediaAluno = 50;
+		String nome = "Fabio";
+
+	if (mediaAluno >= 70 && nome.equals("Alex")) {
+			System.out.println("Parabéns você esta aprovado");
+		} else if (mediaAluno < 70) {
+			System.out.println("Você esta reprovado");
+		}else {
+			System.out.println("Aluno não encontrado");
+			
+		}
+
+	}
+
+}
